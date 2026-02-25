71f82f05b2e8697b051af37300e14b6f8346bb5f
oil653 <oil01@proton.me>
2026-02-25 09:08:16 +0100

    Set up slint, and stuff
    Feeling cute and shiii-

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("Hello word!!!!!!!!!!!!!!!!")
+import slint
+
+class App(slint.loader.ui.main.MainWindow):
+    pass
+
+thing: App = App()
+thing.show()
+thing.run()
+
 raise MemoryError("Femboys are peak")
